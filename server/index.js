b6037ad346fb556551fd35bd7651a4990f13564b
Charlie Hipps <charles.hipps@duke.edu>
2022-06-09 09:19:33 -0700

    working tic-tac-toe version

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-const state = { history: [] };
+let data = {"squares":[null,null,null,null,null,null,null,null,null], "stepNumber":0};
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(bodyParser.json());
 
 // Handle POST requests to /game
 app.post("/game", (req, res) => {
-    state = req.body;
+    data = req.body;
+    res.end;
 });
 
 // Handle GET requests to /game
 app.get("/game", (req, res) => {
-  res.json(state);
+    res.json(data);
 });
 
 // All other GET requests not handled before will return our React app
